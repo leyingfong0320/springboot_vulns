771c33bc6c9945f71ab26df051731ec1f2cbcf86
Frannie <Frannie@gmail.com>
2023-10-31 18:31:28 +0800

    hotfix1 commit

--- src/test/java/com/springboot/test/Controllers/HelloController.java
+++ src/test/java/com/springboot/test/Controllers/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String handle01(){
-        System.out.println("test33322");
+        System.out.println("test333229999");
         return "Hello, Spring Boot!!!!";
     }
 }
